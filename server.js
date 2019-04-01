b40caa5ede4fd707e251dd5a059f9d0e5d5f474d
Chris <chrisem816@gmail.com>
2019-03-31 21:00:43 -0700

    init

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
